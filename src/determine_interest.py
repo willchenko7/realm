769937b944ed2c4a391200745c76798d3d6ba2a6
Will Khomtchenko <willchenko7@gmail.com>
2024-01-11 09:49:31 -0500

    added an interest fitness fn. which is a model trained on users preferences to predict whether a sentence is interesting or not

---
+++ src/determine_interest.py
@@ -0,0 +1,17 @@
+'''
+goal: create a moel that will determine the interest of a user
+- starts off random
+- updates weights each time it recieves feedback from the user
+
+'''
+import numpy as np
+import pickle
+import os
+from forward import forward_with_attention
+
+def determine_interest(tokenized_sentence):
+    model_name = 'interest_model'
+    model = pickle.load(open(os.path.join('models',model_name + '.pkl'),'rb'))
+    w, b, attn_weights, attn_query, attn_keys, attn_values = model
+    output = forward_with_attention(tokenized_sentence, w, b, 5, attn_weights, attn_query, attn_keys, attn_values)
+    return output
---
+++ src/interest_fitness.py
@@ -0,0 +1,40 @@
+'''
+goal: determine fitness of interest model based on latest feedback
+'''
+import numpy as np
+import pickle
+import os
+from forward import forward_with_attention
+from csv_helpers import csv2lol
+from customTokenizer import loadCustomTokenizer
+
+def interest_fitness(interest_model,model_name):
+    tokenizer_path = os.path.join("data","my_tokenizer")
+    tokenizer,data_collator = loadCustomTokenizer(tokenizer_path)
+    w, b, attn_weights, attn_query, attn_keys, attn_values = interest_model
+    #load latest feedback
+    feedback = csv2lol(os.path.join('labeled_data',model_name + '.csv'))
+    #label is in first column, sentence is in second column
+    total_error = 0
+    for label, sentence in feedback:
+        #tokenize sentence
+        tokenized_sentence = tokenizer.encode(sentence)
+        pad_token_id = tokenizer.pad_token_id
+        input_size = 1000
+        x = np.array([pad_token_id for _ in range(input_size)])
+        #replace last n tokens with tokenized sentence
+        x[-len(tokenized_sentence):] = tokenized_sentence
+        #forward pass
+        output = forward_with_attention(x, w, b, 5, attn_weights, attn_query, attn_keys, attn_values)
+        #calculate error
+        error = abs(float(label) - float(output))
+        total_error += error
+    if np.isnan(total_error):
+        total_error = 10000
+    return total_error
+
+if __name__ == '__main__':
+    model_name = 'model3'
+    model = pickle.load(open(os.path.join('models','interest_model' + '.pkl'),'rb'))
+    fitness = interest_fitness(model,model_name)
+    print(fitness)
