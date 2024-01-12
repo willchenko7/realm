bf53fe101aca03de46000225ff2454097c9e8cc4
Will Khomtchenko <willchenko7@gmail.com>
2024-01-12 10:14:42 -0500

    changed diversity fitness fn to chack for diversity for the whole batch of sentences, not just one

--- src/determine_diversity.py
+++ src/determine_diversity.py
@@ -1,6 +1,10 @@
 
 
-def determine_diversity(tokenized_sentence):
-    #get the number of unique tokens in the sentence
-    unique_tokens = set(tokenized_sentence)
+def determine_diversity(tokenized_sentences):
+    #get all unique tokens in a list of tokenized sentences
+    unique_tokens = []
+    for sentence in tokenized_sentences:
+        for token in sentence:
+            if token not in unique_tokens:
+                unique_tokens.append(token)
     return len(unique_tokens)
